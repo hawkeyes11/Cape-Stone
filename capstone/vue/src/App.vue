2a2ddf5fd34f6b08ff3f18c53e11f851da939216
Kyle Youngblood <kyoungbl21@gmail.com>
2022-04-12 22:23:39 -0400

    Added design for login, register and cards

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -13,12 +13,20 @@
 
 <style>
 html {
-  font-size: 16px;
+  font-size: 18px;
   font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  /* font-style: oblique ; */
+  color: #8C1184;
 }
 body {
   display: flex;
-  background-color: whitesmoke;
+  background-color: #AC8A6D;
+  justify-content: center;
+  height: 50vh;
+}
+div#nav {
+  font-family: monospace;
+  font-size: 16px;
 }
 
 /* @media (min-width: 1024px) {
@@ -51,7 +59,26 @@ body {
       "footer";
   } */
 
+/* Color Theme Swatches in Hex */
+.color-theme_DSC_3862-1-hex { color: #888888; }
+.color-theme_DSC_3862-2-hex { color: #CDA787; }
+.color-theme_DSC_3862-3-hex { color: #FBF2EB; }
+.color-theme_DSC_3862-4-hex { color: #AC8A6D; }
+.color-theme_DSC_3862-5-hex { color: #E6CCB6; }
 
+/* Color Theme Swatches in RGBA */
+.color-theme_DSC_3862-1-rgba { color: rgba(216, 198, 175, 1); }
+.color-theme_DSC_3862-2-rgba { color: rgba(205, 167, 135, 1); }
+.color-theme_DSC_3862-3-rgba { color: rgba(251, 242, 235, 1); }
+.color-theme_DSC_3862-4-rgba { color: rgba(172, 138, 109, 1); }
+.color-theme_DSC_3862-5-rgba { color: rgba(230, 204, 182, 1); }
+
+/* Color Theme Swatches in HSLA */
+.color-theme_DSC_3862-1-hsla { color: hsla(33, 34, 76, 1); }
+.color-theme_DSC_3862-2-hsla { color: hsla(27, 41, 66, 1); }
+.color-theme_DSC_3862-3-hsla { color: hsla(26, 66, 95, 1); }
+.color-theme_DSC_3862-4-hsla { color: hsla(27, 27, 55, 1); }
+.color-theme_DSC_3862-5-hsla { color: hsla(27, 48, 80, 1); }
 
 
 </style>
--- capstone/vue/src/views/Register.vue
+++ capstone/vue/src/views/Register.vue
@@ -32,48 +32,52 @@
         v-model="user.confirmPassword"
         required
       />
-      <router-link :to="{ name: 'login' }">Have an account?</router-link>
-      <button @click="checkPassword" class="btn btn-lg btn-primary btn-block" type="submit">
+      <button
+        @click="checkPassword"
+        class="btn btn-lg btn-primary btn-block"
+        type="submit"
+      >
         Create Account
       </button>
+      <router-link :to="{ name: 'login' }">Have an account?</router-link>
     </form>
   </div>
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
 
 export default {
-  name: 'register',
+  name: "register",
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "user",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
-      validPassword: false
+      registrationErrorMsg: "There were problems registering this user.",
+      validPassword: false,
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
-      } else if(!this.validPassword) {
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
+      } else if (!this.validPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Your password does not match the criteria';
+        this.registrationErrorMsg = "Your password does not match the criteria";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -81,25 +85,102 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
     checkPassword() {
       const validation = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-      if(this.user.password.match(validation)) {
+      if (this.user.password.match(validation)) {
         this.validPassword = true;
       } else {
         this.validPassword = false;
       }
-    }
+    },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+#register {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: 35rem;
+  justify-content: center;
+  align-items: center;
+  margin: 4rem auto;
+}
+form.form-register {
+  display: flex;
+  flex-direction: column;
+  background-color: #fbf2eb;
+  width: 750px;
+  border-radius: 15px;
+  padding: 2.5%;
+  height: 100%;
+}
+label {
+  display: flex;
+  padding: 10px;
+  font-size: 24px;
+}
+label.username {
+  display: flex;
+  margin: 2%;
+}
+input#username {
+  display: flex;
+  margin-left: 2%;
+  margin-bottom: 2%;
+  width: 50%;
+  padding: 10px;
+  font-family: monospace;
+  font-size: 16px;
+}
+label.password {
+  display: flex;
+  margin: 2%;
+}
+input#password {
+  display: flex;
+  margin-left: 2%;
+  margin-bottom: 2%;
+  width: 50%;
+  padding: 10px;
+  font-family: monospace;
+}
+label.confirmPassword {
+  display: flex;
+  margin: 2%;
+}
+input#confirmPassword {
+  display: flex;
+  margin-left: 2%;
+  width: 50%;
+  padding: 10px;
+  margin-bottom: 5%;
+  font-family: monospace;
+}
+form#login {
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  display: flex;
+  width: 50%;
+}
+button {
+  width: 75%;
+  justify-content: center;
+  align-content: center;
+  margin-top: 10px;
+  background-image: radial-gradient(#ff0821, #ff5465);
+  padding: 15px;
+  border-radius: 5px;
+  margin-bottom: 2%;
+  font-size: 18px;
+}
+</style>
