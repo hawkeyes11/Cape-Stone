d53cd2f7eba3dfe616c467d38ed8ac3afc8345df
Andrew Pinner <andppinner@gmail.com>
2022-04-14 16:55:49 -0400

    Back end complete for user invites, prefrences, and categories, framing for user favorite restuarant list

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -2,7 +2,8 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS groups;
-DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS group_favorites;
+DROP TABLE IF EXISTS user_favorites;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
 CREATE SEQUENCE seq_user_id
@@ -28,11 +29,25 @@ CREATE TABLE groups (
     CONSTRAINT group_id PRIMARY KEY (group_id)
 );
 
-CREATE TABLE favorites (
-    group_id int,
-    restaurant_id varchar(69),
+CREATE TABLE group_favorites (
+    group_id int NOT NULL,
+    restaurant_id varchar(69) NOT NULL,
 
     FOREIGN KEY (group_id) REFERENCES groups(group_id)
+);
+
+CREATE TABLE user_favorites (
+    user_id int NOT NULL,
+    restaurant_id varchar(69) NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE user_categories (
+    user_id int NOT NULL,
+    category varchar(69) NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
