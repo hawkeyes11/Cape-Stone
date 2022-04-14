ed4eb39c9e93f94d5fa043c406a3a47ef47a2ee2
Mikey Stewart <mjstewa92@gmail.com>
2022-04-14 12:04:01 -0400

    added favorites and groups to database

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,6 +1,8 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS favorites;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
 CREATE SEQUENCE seq_user_id
@@ -16,6 +18,21 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE groups (
+    group_id int NOT NULL UNIQUE,
+    location varchar(50) NOT NULL,
+    expiration_date DATE NOT NULL,
+
+    CONSTRAINT group_id PRIMARY KEY (group_id)
+);
+
+CREATE TABLE favorites (
+    group_id int,
+    restaurant_id varchar(69),
+
+    FOREIGN KEY (group_id) REFERENCES groups(group_id)
 );
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
